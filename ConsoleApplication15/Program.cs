20990a7e4ddc43dfb0790627f21e2cffed1291a1
tsimmons15 <tsimmons15@valenciacollege.edu>
2019-01-28 11:04:14 -0500

    Initial commit

---
+++ ConsoleApplication15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlServerCe;
+using System.IO;
+
+namespace ConsoleApplication15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString;
+            string fileName = "db.sdf";
+            string password = "db";
+            
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+            connectionString = string.Format("DataSource =\"{0}\"; Password = '{1}'", fileName, password);
+
+            SqlCeEngine en = new SqlCeEngine(connectionString);
+            en.CreateDatabase();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# SQL Server CE Test
